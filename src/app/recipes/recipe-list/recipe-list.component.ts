58317e71b7f4b05502eb79ba91d9f0614f544625
Sarder-Iftekhar <iftekhar.sarder@gmail.com>
2022-06-21 18:02:18 +0600

    My Fast application

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+
+    new Recipe('Pizza',
+     'Very much testy',
+      'https://thumbs.dreamstime.com/z/various-spices-recipe-word-vintage-background-copyspace-selective-focus-food-baking-cooking-concept-97076702.jpg')
+    ,
+      new Recipe('Burger',
+      'Very much testy',
+       'https://thumbs.dreamstime.com/z/various-spices-recipe-word-vintage-background-copyspace-selective-focus-food-baking-cooking-concept-97076702.jpg'),
+       new Recipe('Sandwitch',
+     'Very much testy',
+      'https://thumbs.dreamstime.com/z/various-spices-recipe-word-vintage-background-copyspace-selective-focus-food-baking-cooking-concept-97076702.jpg'),
+      new Recipe('A testy recipe',
+     'Very much testy',
+      'https://thumbs.dreamstime.com/z/various-spices-recipe-word-vintage-background-copyspace-selective-focus-food-baking-cooking-concept-97076702.jpg')
+
+
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
